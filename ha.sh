4e0f0feeabaf1844e03338d5dac33d2f71a20d38
Abderrazak BOUADMA <abderrazak.bouadma@kyriba.com>
2020-01-07 16:26:00 +0100

    clean

--- ha.sh
+++ ha.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -p443:443 -p80:80 -p1936:1936 -v /Users/abderrazak.bouadma/job/zoubia/cert:/etc/certs -v /Users/abderrazak.bouadma/job/zoubia/haproxy/haproxy.cfg:/etc/haproxy/haproxy.cfg --network ha --rm --name ilb haproxy:1.9.10 haproxy -f /etc/haproxy/haproxy.cfg
+docker run -it -p443:443 -p80:80 -p1936:1936 -v $(pwd)/certs:/etc/certs -v $(pwd)/cfg/haproxy.cfg:/etc/haproxy/haproxy.cfg --network ha --rm --name ilb haproxy:1.9.10 haproxy -f /etc/haproxy/haproxy.cfg
